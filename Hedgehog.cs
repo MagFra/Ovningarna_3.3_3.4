d3682fc3c5fe0c0f87f0eec81f3020776ee664c6
MagFra <47413425+magfra@users.noreply.github.com>
2023-09-25 11:57:49 +0200

    Igelkott

---
+++ Hedgehog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ovningarna_3._3_3._4
+{
+    internal class Hedgehog : Animal
+    {
+        int nrOfSpikes;
+        public Hedgehog(string namn, int vikt, int alder, int nrOfSpikes = 10000) : base(namn, vikt, alder) => this.nrOfSpikes = nrOfSpikes;
+        public override void DoSound()
+        {
+            Console.WriteLine("Fräs!");
+        }
+        public int NrOfSpikes() => nrOfSpikes;
+    }
+}
