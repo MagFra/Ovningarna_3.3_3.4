f1e1e01e48d2056f62fa574bd8d444f7ec84607d
MagFra <47413425+magfra@users.noreply.github.com>
2023-09-25 10:34:45 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Ovningarna_3._3_3._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            // Övning 3.3) Arv
+            // 13.F: Om vi under utvecklingen kommer fram till att samtliga fåglar behöver ett nytt
+            // attribut, i vilken klass bör vi lägga det?
+            // Svar: Bird.
+            // 14.F: Om alla djur behöver det nya attributet, vart skulle man lägga det då?
+            // Svar: Animal.
+
+
+        }
+    }
+}
