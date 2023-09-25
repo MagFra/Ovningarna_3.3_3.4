1d38b9d7f114b661883b3924ebba83c4fef0d848
MagFra <47413425+magfra@users.noreply.github.com>
2023-09-25 12:03:06 +0200

    Orm

---
+++ Worm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ovningarna_3._3_3._4
+{
+    internal class Worm: Animal
+    {
+        bool isPoisonous;
+        public Worm(string namn, int vikt, int alder, bool isPoisonous = false) : base(namn, vikt, alder) => this.isPoisonous = isPoisonous;
+        public override void DoSound()
+        {
+            Console.WriteLine("Väs!");
+        }
+        public bool IsPoisonous() => isPoisonous;
+    }
+}
